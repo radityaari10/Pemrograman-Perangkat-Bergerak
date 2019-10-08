cb291cd5be4c1601e53d17742512c49c3afd8b32
radityaari10 <radityaari10@gmail.com>
2019-10-08 23:48:14 +0700

    Modul 4 Project 1 Ver 2

--- Modul_4.1-SignUpApp/SignUpApp-Modul4/app/src/main/java/com/rapandroid/signupapp/LoginActivity.java
+++ Modul_4.1-SignUpApp/SignUpApp-Modul4/app/src/main/java/com/rapandroid/signupapp/LoginActivity.java
@@ -2,13 +2,32 @@ package com.rapandroid.signupapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    Button BtnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        BtnRegister = findViewById(R.id.BtnGabung);
+        BtnRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.BtnGabung:
+                Intent register = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(register);
+                break;
+        }
+
     }
 }
