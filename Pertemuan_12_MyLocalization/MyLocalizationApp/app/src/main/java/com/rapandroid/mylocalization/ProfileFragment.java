9cbcaf214cfb37f6f48ab6533a50bd39ee9a800d
radityaari10 <radityaari10@gmail.com>
2019-11-26 22:38:40 +0700

    Pertemuan 12 MyLocalization Dengan Profil

---
+++ Pertemuan_12_MyLocalization/MyLocalizationApp/app/src/main/java/com/rapandroid/mylocalization/ProfileFragment.java
@@ -0,0 +1,38 @@
+package com.rapandroid.mylocalization;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileFragment extends Fragment {
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+        //ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_profile, null);
+
+    }
+
+
+
+}
